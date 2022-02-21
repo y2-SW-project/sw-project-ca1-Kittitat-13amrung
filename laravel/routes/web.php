1af32f02627a8601e0665bd5604e558205f3641d
Kittiat Bamrung <n0020132@iadt.ie>
2022-02-21 20:51:54 +0000

    Added Wireframes

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -20,6 +20,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/chat' ,function() {
+    return view('counter');
+});
+
 // Authentication to check for roles
 Auth::routes();
 
